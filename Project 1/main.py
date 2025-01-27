411fb4a9793f892e6d6693044d4c28d41b5fb127
hr-shuvo <h.r.shuvo9@gmail.com>
2025-01-27 23:44:52 +0600

    load data for single page

--- Project 1/main.py
+++ Project 1/main.py
@@ -2,7 +2,34 @@ import pandas as pd
 import numpy as np
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
+
+# store unique length of row // it will help to know if there are any missing data 
+unique_lengths = set()
+book_details = []
+
+def get_book_details(book):
+    global unique_lengths
+
+    details = book.text.split('\n')
+    # title = book.find_element(By.CLASS_NAME, 'bookTitle').text
+    unique_lengths.add(len(details))
+
+    if len(details) == 12:
+        details.insert(3, '')
+
+    content = {}
+
+    content['Rank']  = details[0]
+    content['Title'] = details[1]
+    content['Author']= details[2]
+    content['Rating']= details[4]
+    
+    
+
+    return content
+
 
 
 
@@ -11,6 +38,27 @@ def main():
 
     driver = webdriver.Chrome()
     driver.get(url=url)
+
+    columns = ['Rank', 'Title', 'Author', 'Rating']
+
+    data_table = driver.find_element(By.ID, 'all_votes')
+
+    table_body = data_table.find_element(By.XPATH, './table/tbody')
+
+    rows = table_body.find_elements(By.XPATH, './tr')
+
+
+    for idx, row in enumerate(rows):
+        book_details.append(get_book_details(row))
+
+    # print('different types of length (tr): ', unique_lengths)
+
+
+    df = pd.DataFrame(data=book_details, columns=columns)
+    print(df.head())
+    df.to_csv('data.csv', index=False)
+
+    
 
 
 
