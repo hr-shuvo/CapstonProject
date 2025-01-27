bb11c18f96698732f0d507bd66f6d0f2ac818c38
hr-shuvo <h.r.shuvo9@gmail.com>
2025-01-27 19:33:46 +0600

    load url page on browser

--- Project 1/main.py
+++ Project 1/main.py
@@ -1,6 +1,23 @@
+import pandas as pd
+import numpy as np
+
+from selenium import webdriver
+import time
 
 
 
+def main():
+    url = 'https://www.goodreads.com/list/show/1.Best_Books_Ever'
+
+    driver = webdriver.Chrome()
+    driver.get(url=url)
 
 
-print('hi... ')
+
+    time.sleep(10)
+    driver.close()
+
+
+
+if __name__ == '__main__':
+    main()
